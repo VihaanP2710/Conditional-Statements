9546468d3ee09b9904260bd31e08e28f8e0c42b0
VihaanP2710 <v.4956194@gmail.com>
2025-03-22 14:39:40 +0530

    added greatorsmall.py

---
+++ greaterorsmaller.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a Number: "))
+if (i < 15):
+    print("i is smaller than 15")
+    print("i'm in if block")
+else:
+    print("i is greater than 15")
+    print("i'm in else block")
+print("I'm not in if or else block")
