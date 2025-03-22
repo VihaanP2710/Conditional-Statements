085e4a894b694918c30fd44bd7460c530b871142
VihaanP2710 <v.4956194@gmail.com>
2025-03-22 14:49:33 +0530

    added evenorodd.py

---
+++ evenorodd.py
@@ -0,0 +1,8 @@
+num = int(input("Enter number to check: "))
+print("Number to be checked: ", num)
+
+if num % 2 == 0:
+    print("This is an Even Number.")
+
+else:
+    print("This is an Odd Number.")
